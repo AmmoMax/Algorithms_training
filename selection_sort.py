b63296403b4c9d2abbb77d7fbf9186908a4f5a68
Max <steelratmax@gmail.com>
2019-10-26 22:18:51 +0300

    add simple selection sort sample

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,30 @@
 # coding: utf-8
 
 # простейшая реализация алгоритма сортировки выбором.
+# Simple Selection Sort Algorithm
+
+test_arr = [23, 9, 232, 685, 3, 454, 12]
+test_arr2 = []
+test_arr3 = [23]
 
 
 def find_smallest(arr):  # функция поиска индекса наименьшего элемента в массиве
     smallest = arr[0]
     smallest_index = 0
+
     for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
-            smallest_index += i
+            smallest_index = i
     return smallest_index
 
-test_arr = [23, 9, 232, 685, 3, 454, 12]
-print(find_smallest(test_arr))
+
+def selectionSort(arr):  # функция сортировки выбором
+    resarr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)  # находим наименьший элемент входного массива
+        resarr.append(arr.pop(smallest))  # добавляем наименьший элемент в финальный массив и удаляем из входного массива
+    return resarr
+
+
+print(selectionSort(test_arr3))
